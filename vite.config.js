79aa3d2114a0d399bda665b1696b8610edd18cac
tvinchent <tvinchent@gmail.com>
2025-11-21 07:18:26 +0100

    test deployment

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/pictionIA",
+  base: "/pictionIA/dist/",
   plugins: [vue()],
   resolve: {
     alias: {
