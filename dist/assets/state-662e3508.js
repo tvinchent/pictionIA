557016ab6923564df8de5b20e7aae5a746ac86f4
tvinchent <tvinchent@gmail.com>
2025-11-21 05:45:43 +0100

    change baseURL

--- dist/assets/state-662e3508.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-20a46d4c.js";const s=r({questionNumber:1,pourcentQuestions:Array,promptAi:"",bestScore:0,arrayQuestionsNumbers:[]});export{s};
---
+++ dist/assets/state-eb42c072.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-d9ef7283.js";const s=r({questionNumber:1,pourcentQuestions:Array,promptAi:"",bestScore:0,arrayQuestionsNumbers:[]});export{s};
